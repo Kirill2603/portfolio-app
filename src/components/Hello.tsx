ce82fc18b9af0934ac63c56a5d8906acff544c9a
kirill2603 <k.radobolsky@gmail.com>
2023-07-31 11:07:00 +0300

    + min change text

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -9,7 +9,7 @@ export const Hello = () => {
         Hello!
       </Code>
       <Code p={2} mb={2} colorScheme={'green'} fontSize={'20px'}>
-        I'm Front End Web Developer based in Minsk
+        I'm software Developer based in Minsk
       </Code>
       <Alert status='success' variant='solid'>
         <AlertIcon />
@@ -17,9 +17,11 @@ export const Hello = () => {
       </Alert>
       <Flex pt={10} justify={'space-between'} pb={5}>
         <Box pr={5}>
-          <Text fontSize='3xl'>Front-end developer</Text>
+          <Text fontSize='3xl'>Front-end, mobile developer</Text>
           <Text fontSize='2xl' wordBreak={'break-word'} width={'80%'}>
             With experience in creating Landing Pagesand SPA, using React(JS/TS), Redux, AXIOS/REST API</Text>
+          <Text fontSize='2xl' wordBreak={'break-word'} width={'80%'}>
+            And mobile apps, using React Navite / Flutter</Text>
         </Box>
         <Image p={0.5} rounded='full' w='20vh' h='20vh' src={myPhoto} />
       </Flex>
